511396c4a37054516acfeab97ab0a23c4edf38e0
Daniel Storey <dstorey@spartaglobal.com>
2018-11-08 15:18:58 +0000

    tests pass

--- spec/budget_watch_spec.rb
+++ spec/budget_watch_spec.rb
@@ -14,4 +14,27 @@ describe "Testing Budget Watch App" do
   it "should open the app to the homepage" do
     expect(find_element(:id, "protect.budgetwatch:id/action_settings"))
   end
+
+  it 'should have the budget option on the main landing page' do
+    expect(find_element(:id, "protect.budgetwatch:id/menu").displayed?).to eq true
+  end
+
+  it 'should access the budget page' do
+    find_element(:id, "protect.budgetwatch:id/menu").click
+    expect(find_element(:id, "protect.budgetwatch:id/action_calendar").displayed?).to eq true
+    expect(find_element(:id, "protect.budgetwatch:id/action_add").displayed?).to eq true
+  end
+
+  it 'should be able to add a budget' do
+    find_element(:id, "protect.budgetwatch:id/action_add").click
+    find_element(:id, "protect.budgetwatch:id/budgetNameEdit").send_keys("Brian Moneeez")
+    find_element(:id, "protect.budgetwatch:id/valueEdit").send_keys("2547")
+    find_element(:id, "protect.budgetwatch:id/action_save").click
+
+    expect(find_element(:id, "protect.budgetwatch:id/budgetName").displayed?).to eq true
+    find_element(:id, "protect.budgetwatch:id/budgetName").click
+    expect(find_element(:id, "protect.budgetwatch:id/helpText").displayed?).to eq true
+    expect(find_element(:id, "protect.budgetwatch:id/pager").displayed?).to eq true
+  end
+
 end
